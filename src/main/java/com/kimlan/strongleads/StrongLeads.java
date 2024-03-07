f450ff651596824a43e0a465c8a620cded888940
Kim-Lan <83447448+Kim-Lan@users.noreply.github.com>
2024-03-07 11:35:30 -0800

    initial commit

---
+++ src/main/java/com/kimlan/strongleads/StrongLeads.java
@@ -0,0 +1,22 @@
+package com.kimlan.strongleads;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StrongLeads implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger("strongleads");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Strong Leads initialized!");
+	}
+}
